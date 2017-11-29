cadda97d1ff517f84f0fe2c9970513814416b59d
SOliver93 <sasa.oliver@vsmti.hr>
2017-11-29 20:49:47 +0100

    Upload Mail Sender

---
+++ MailSender/TestMailSender/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MailSender;
+
+namespace TestMailSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MailClient Klijent = new MailClient();
+            Klijent.SendEmail("sasa.oliver@vsmti.hr", "Poruka", "Test");
+            Console.ReadKey();
+        }
+    }
+}
